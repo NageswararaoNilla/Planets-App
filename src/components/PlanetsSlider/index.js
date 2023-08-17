c0f5ca377dd1db0e45895b8dadcce6e91e1c6e64
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-08-17 07:00:01 +0000

    added functionlities

--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -1,1 +1,35 @@
-// Write your code here
+import Slider from 'react-slick'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+  const settings = {
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+  return (
+    <div className="carousel-container">
+      <div className="slider-container" data-testid="planets">
+        <Slider {...settings}>
+          {planetsList.map(eachPlanet => (
+            <PlanetItem planetDetails={eachPlanet} key={eachPlanet.id} />
+          ))}
+        </Slider>
+      </div>
+    </div>
+  )
+}
+
+export default PlanetsSlider
+
+//
+//   <div className="slider-container" >
+//     <Slider {...settings}>
+//
+//     </Slider>
+//   </div>
+// </div>
